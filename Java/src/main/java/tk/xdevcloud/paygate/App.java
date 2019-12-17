d5a2f8b77e5950bcbbc25f9863615a9aa0910ba3
William <kwesidev@gmail.com>
2019-12-17 17:50:08 +0200

    remove

--- Java/src/main/java/tk/xdevcloud/paygate/App.java
+++
@@ -1,9 +0,0 @@
-package tk.xdevcloud.paygate;
-public class App {
-	
-    public static void main(String[] args) {
-    	
-       PayGateWeb pweb = new PayGateWeb();
-       pweb.doRequest();
-    }
-}
