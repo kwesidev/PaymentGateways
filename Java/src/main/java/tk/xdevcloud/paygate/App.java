6cdb819d939c4536f3b035c41aba584af17f92f8
William <kwesidev@gmail.com>
2019-12-17 17:43:40 +0200

    init

---
+++ Java/src/main/java/tk/xdevcloud/paygate/App.java
@@ -0,0 +1,9 @@
+package tk.xdevcloud.paygate;
+public class App {
+	
+    public static void main(String[] args) {
+    	
+       PayGateWeb pweb = new PayGateWeb();
+       pweb.doRequest();
+    }
+}
