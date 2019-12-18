f67a49eba449cae5211fde55dcc99d239aa6a46e
William <kwesidev@gmail.com>
2019-12-18 09:30:20 +0200

    add PayGateException class

---
+++ Paygate_Java/src/main/java/tk/xdevcloud/paygate/PayGateException.java
@@ -0,0 +1,15 @@
+package tk.xdevcloud.paygate;
+
+@SuppressWarnings("serial")
+public class PayGateException extends Exception{
+	
+	/**
+	 * PayGateException
+	 * @param message
+	 */
+	public PayGateException(String message) {
+		
+		super(message);
+	}
+
+}
